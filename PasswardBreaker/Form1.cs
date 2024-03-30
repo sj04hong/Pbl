d501de6ad3870b47fa0525ca740544d61e19f396
sj04hong <sh04hong@gmail.com>
2024-03-30 13:34:15 +0900

    Pbl

--- PasswardBreaker/Form1.cs
+++ PasswardBreaker/Form1.cs
@@ -1,7 +1,8 @@
 ﻿using Ionic.Zip;
 using Microsoft.WindowsAPICodePack.Dialogs;
+using System;
+using System.Collections.Generic;
 using System.IO;
-using System.IO.Compression;
 using System.Linq;
 using System.Windows.Forms;
 
@@ -22,14 +23,14 @@ namespace PasswardBreaker
         {
             //try
             //{
-                CommonOpenFileDialog fileDialog= new CommonOpenFileDialog();
+            CommonOpenFileDialog fileDialog = new CommonOpenFileDialog();
 
-                if(fileDialog.ShowDialog() == CommonFileDialogResult.Ok)
-                {
-                    this.txtBox_Path.Text = fileDialog.FileName;
-                    
-                    Decrypt(fileDialog.FileName);
-                }
+            if (fileDialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                this.txtBox_Path.Text = fileDialog.FileName;
+
+                Decrypt(fileDialog.FileName);
+            }
             //}
             //catch
             //{
@@ -39,20 +40,89 @@ namespace PasswardBreaker
 
         private void Decrypt(string zipFileName/*, string unZipFileName*/)
         {
-            string unZipFileName = "C:\\Users\\USER\\Downloads\\새 폴더\\";
+            //string unZipFileName = "C:\\Users\\USER\\Downloads\\새 폴더\\";
             int[] numberList = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
             var upperText = Enumerable.Range('A', 26).Select(x => ((char)x).ToString().ToUpper());
             var lowerText = Enumerable.Range('A', 26).Select(x => ((char)x).ToString().ToLower());
             var specialText = Enumerable.Range(33, 15).Select(x => ((char)x).ToString());
 
-            using (ZipFile zip = new ZipFile(zipFileName))
+            //string fileName = zipFileName.Substring(0, zipFileName.LastIndexOf(".")).Split(new string[] { "\\", "" }, StringSplitOptions.None).Last();
+            
+            string unZipFileName = zipFileName.Substring(0, zipFileName.LastIndexOf("."));
+            Random random = new Random();
+
+            if (!Directory.Exists(unZipFileName))
             {
-                if (!Directory.Exists(unZipFileName))
-                {
-                    zip.Password = "1234";
-                    zip.ExtractAll(unZipFileName);  
-                }
+                Directory.CreateDirectory(unZipFileName);
             }
+            int length = 4;
+
+            double passwordCase = Math.Pow(numberList.Length, length) * Math.Pow(upperText.Count(), length) *
+                                Math.Pow(lowerText.Count(), length) * Math.Pow(specialText.Count(), length);
+
+            string combination = string.Empty;
+            HashSet<string> duplicates = new HashSet<string>();
+            //for (int i = 0; i < passwordCase; i++)
+            int i = 0;
+            while (i < passwordCase)
+            {
+                if (duplicates.Contains(combination))
+                {
+                    combination = string.Empty;
+                    continue;
+                }
+                try
+                {
+                    if (!string.IsNullOrEmpty(combination))
+                    {
+                        using (ZipFile zip = ZipFile.Read(zipFileName))
+                        {
+                            zip.Password = combination;
+                            duplicates.Add(combination);
+
+                            foreach (var entry in zip)
+                            {
+                                entry.Extract(unZipFileName, ExtractExistingFileAction.OverwriteSilently);
+                            }
+                            MessageBox.Show("Complete");
+                            break;
+                        }
+
+                    }
+
+
+                    for (int j = 0; j < length; j++)
+                    {
+                        int listIndex = random.Next(0); // numberList, upperText, lowerText, specialText 중 하나의 리스트 선택
+
+                        switch (listIndex)
+                        {
+                            case 0:
+                                combination += numberList[random.Next(numberList.Length)];
+                                break;
+                            case 1:
+                                combination += upperText.ElementAt(random.Next(upperText.Count()));
+                                break;
+                            case 2:
+                                combination += lowerText.ElementAt(random.Next(lowerText.Count()));
+                                break;
+                            case 3:
+                                combination += specialText.ElementAt(random.Next(specialText.Count()));
+                                break;
+                        }
+                    }
+                }
+                catch { i++; }
+            }
+            //using (ZipFile zip = ZipFile.Read(zipFileName))
+            //{
+            //    zip.Password = "1234";
+
+            //    foreach (var entry in zip)
+            //    {
+            //        entry.Extract(unZipFileName, ExtractExistingFileAction.OverwriteSilently);
+            //    }
+            //}   
         }
     }
 }
